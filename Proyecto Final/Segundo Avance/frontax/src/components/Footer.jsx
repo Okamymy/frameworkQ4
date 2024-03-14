77fab20e530ac7522ad8b0a4d74b6e3c91fe8ca4
Okamymy <135782002+Okamymy@users.noreply.github.com>
2024-03-14 08:59:13 -0700

    Delete Proyecto Final/Segundo Avance directory

--- Proyecto Final/Segundo Avance/frontax/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(){
-    return(
-        <footer id="foo">
-            <p>© 2024 Axolotl Group. Todos los derechos reservados.</p>
-        </footer>
-    );
-}
